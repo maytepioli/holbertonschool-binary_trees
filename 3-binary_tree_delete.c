8e4f80c37c5a37334e27ce35945919e83a03f301
Luna Leguisamo <9356@holbertonstudents.com>
2024-08-05 19:38:10 +0000

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes
+ * an entire binary tree
+ * @tree: Pointer to the root node
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
