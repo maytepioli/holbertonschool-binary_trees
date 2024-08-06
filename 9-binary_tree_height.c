b12a62c908f8645696c3a50454e5dbe7c3048c61
Luna Leguisamo <9356@holbertonstudents.com>
2024-08-06 20:12:49 +0000

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,22 +9,34 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t counter = 0;
+	size_t counter;
+	size_t counter2;
 
 	if (tree == NULL)
 		return (0);
 
-	while (tree != NULL)
+	if (tree->left)
 	{
-		tree = tree->left;
-		counter++;
+		counter = 1 + binary_tree_height(tree->left);
+	}
+	else
+	{
+		counter = 0;
 	}
 
-	while (tree != NULL)
+	if (tree->right)
 	{
-		tree = tree->right;
-		counter++;
+		counter2 = 1 + binary_tree_height(tree->right);
+	}
+	else
+	{
+		counter2 = 0;
 	}
 
-	return (counter);
+	if (counter > counter2)
+	{
+		return (counter);
+	}
+		return (counter2);
 }
+
