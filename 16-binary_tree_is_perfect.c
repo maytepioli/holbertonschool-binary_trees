1a026fa4243c368786ec4e7e78f29b119d86fb7c
Luna Leguisamo <9356@holbertonstudents.com>
2024-08-07 18:34:19 +0000

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,29 @@
 #include "binary_trees.h"
-#include "15-binary_tree_is_full.c"
 #include "9-binary_tree_height.c"
+#include "12-binary_tree_leaves.c"
+
+/**
+ * _pow_recursion - function that returns the value of
+ * x raised to the power of y
+ * @x: number base
+ * @y: power of x
+ * Return: the value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (-1);
+}
+
 /**
  * binary_tree_is_perfect - function that checks if
  * a binary tree is perfect
@@ -11,30 +34,12 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t counter;
-	size_t counter2;
+	int n = binary_tree_height(tree);
+	int perfect = _pow_recursion(2, n);
+	int leaves = binary_tree_leaves(tree);
 
-	if (tree == NULL)
-		return (0);
-	if (tree->left)
-	{
-		counter = 1 + binary_tree_height(tree->left);
-	}
-	else
-	{
-		counter = 0;
-	}
-	if (tree->right)
-	{
-		counter2 = 1 + binary_tree_height(tree->right);
-	}
-	else
-	{
-		counter2 = 0;
-	}
-	if (counter == counter2)
-	{
+	if (perfect == leaves)
 		return (1);
-	}
+
 	return (0);
 }
